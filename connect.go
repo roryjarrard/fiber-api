90dd78dc49345ae53e66e2bf8fa980a125c41c30
Rory Jarrard <rjarrard@frontsteps.com>
2022-01-07 14:55:21 -0700

    abstract db connection

---
+++ connect.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+func getDSN() string {
+	godotenv.Load()
+	host := os.Getenv("DBHOST")
+	port := os.Getenv("DBPORT")
+	user := os.Getenv("DBUSER")
+	pass := os.Getenv("DBPASS")
+	name := os.Getenv("DBNAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=America/Denver", host, port, user, pass, name)
+	return dsn
+}
+
+func connectDB() error {
+	dsn := getDSN()
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
