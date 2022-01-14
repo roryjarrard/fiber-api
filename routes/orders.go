937c34f77d658023d752595f1fbae708ba4b8608
Rory Jarrard <rjarrard@frontsteps.com>
2022-01-14 13:00:00 -0700

    create order

--- routes/orders.go
+++ routes/orders.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/roryjarrard/fiber-api/database"
 	"github.com/roryjarrard/fiber-api/models"
 )
 
@@ -19,8 +20,36 @@ func CreateResponseOrder(order models.Order, user User, product Product) Order {
 	return Order{ID: order.ID, User: user, Product: product, CreatedAt: order.CreatedAt}
 }
 
-func CreateOrder(c *fiber.Ctx) error { return nil }
-func GetOrders(c *fiber.Ctx) error   { return nil }
-func GetOrder(c *fiber.Ctx) error    { return nil }
+func CreateOrder(c *fiber.Ctx) error {
+	var order models.Order
+
+	if err := c.BodyParser(&order); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	var user models.User
+	if err := findUser(order.UserRef, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	var product models.Product
+	if err := findProduct(order.ProductRef, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&order)
+
+	responseUser := CreateResponseUser(user)
+	responseProduct := CreateResponseProduct(product)
+	responseOrder := CreateResponseOrder(order, responseUser, responseProduct)
+
+	return c.Status(201).JSON(responseOrder)
+}
+
+func GetOrders(c *fiber.Ctx) error { return nil }
+
+func GetOrder(c *fiber.Ctx) error { return nil }
+
 func UpdateOrder(c *fiber.Ctx) error { return nil }
+
 func DeleteOrder(c *fiber.Ctx) error { return nil }
