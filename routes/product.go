6dd27564d62c65c27f3aa7584b0857199499c4bb
Rory Jarrard <rjarrard@frontsteps.com>
2022-01-14 10:44:44 -0700

    create and get user(s)

--- routes/product.go
+++ routes/product.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/roryjarrard/fiber-api/database"
 	"github.com/roryjarrard/fiber-api/models"
 )
 
@@ -20,8 +23,57 @@ func CreateResponseProduct(productModel models.Product) Product {
 	}
 }
 
-func GetProducts(c *fiber.Ctx) error   { return nil }
-func GetProduct(c *fiber.Ctx) error    { return nil }
-func CreateProduct(c *fiber.Ctx) error { return nil }
+func GetProducts(c *fiber.Ctx) error {
+	var products []models.Product
+
+	if err := database.Database.Db.Find(&products).Error; err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	var responseProducts []Product = []Product{}
+
+	for _, p := range products {
+		responseProducts = append(responseProducts, CreateResponseProduct(p))
+	}
+
+	return c.Status(200).JSON(responseProducts)
+}
+
+func findProduct(id int, product *models.Product) error {
+	database.Database.Db.Find(&product, "id = ?", id)
+	if product.ID == 0 {
+		return errors.New("product does not exist")
+	}
+	return nil
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	id, err := c.ParamsInt("productId")
+	if err != nil {
+		return c.Status(400).JSON("please supply an integer value")
+	}
+
+	if err := findProduct(id, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	responseProduct := CreateResponseProduct(product)
+	return c.Status(200).JSON(responseProduct)
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&product)
+	responseProduct := CreateResponseProduct(product)
+	return c.Status(200).JSON(responseProduct)
+}
+
 func UpdateProduct(c *fiber.Ctx) error { return nil }
 func DeleteProduct(c *fiber.Ctx) error { return nil }
