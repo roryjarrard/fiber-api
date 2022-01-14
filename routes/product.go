c1d316669319327aab7a4041c1ec1d947a8c25c9
Rory Jarrard <rjarrard@frontsteps.com>
2022-01-14 11:01:49 -0700

    update product

--- routes/product.go
+++ routes/product.go
@@ -75,5 +75,40 @@ func CreateProduct(c *fiber.Ctx) error {
 	return c.Status(200).JSON(responseProduct)
 }
 
-func UpdateProduct(c *fiber.Ctx) error { return nil }
+func UpdateProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	id, err := c.ParamsInt("productId")
+	if err != nil {
+		return c.Status(400).JSON("please supply an integer value")
+	}
+
+	if err := findProduct(id, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	type UpdateProduct struct {
+		Name         string `json:"name"`
+		SerialNumber string `json:"serial_number"`
+	}
+
+	var updateData UpdateProduct
+
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if updateData.Name != "" {
+		product.Name = updateData.Name
+	}
+
+	if updateData.SerialNumber != "" {
+		product.SerialNumber = updateData.SerialNumber
+	}
+
+	database.Database.Db.Save(&product)
+	responseProduct := CreateResponseProduct(product)
+	return c.Status(201).JSON(responseProduct)
+}
+
 func DeleteProduct(c *fiber.Ctx) error { return nil }
